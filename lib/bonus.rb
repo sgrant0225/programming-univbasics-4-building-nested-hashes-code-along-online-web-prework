1b7d31659f02c86e97dbf1eafc31c8608122858e
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-05 03:27:28 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,6 +27,6 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
 def bonus
   epic_tragedy[:montague][:hero][:status] = "dead"
-  puts epic_tragedy
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
 end
